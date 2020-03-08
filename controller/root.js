2f05479eb585bf5bf4c29febf938479eca95d4eb
Sukianto Suteja <sukialiong@gmail.com>
2020-03-09 01:13:59 +1100

    root controller code clean up

--- controller/root.js
+++ controller/root.js
@@ -1,22 +1,9 @@
-const axios = require("axios");
-const parseString = require("xml2js").parseString;
 const { rootNSW } = require('../controller/nsw')
 const { rootVIC } = require('../controller/vic')
 const { rootQLD } = require('../controller/qld')
 
-exports.getAll = async (req, res, next) => {
-  // Promise.all([rootNSW, rootVIC, rootQLD]).then(function(values) {
-  //   console.log(values);
-  //   res.send(values);
-  // });
-
-  const nsw = await rootNSW();
-  const vic = await rootVIC();
-  const qld = await rootQLD();
-  console.log({nsw, vic, qld});
-  
+exports.getAll = (req, res, next) => {
   Promise.all([rootNSW(), rootVIC(), rootQLD()]).then(function(values) {
-    console.log(values);
     res.send(values)
   });
 };
